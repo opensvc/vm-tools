2548e9aedca9be60e37c60156c9d82d6e6ec2586
Arnaud Véron <arnaud.veron@opensvc.com>
2026-04-13 16:23:27 +0200

    Fix zfs on debian13

--- build/debian13/scripts/zfs.sh
+++ build/debian13/scripts/zfs.sh
@@ -4,10 +4,8 @@ echo "--- Begin zfs.sh ---"
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt -y update && apt -y install zfsutils-linux
+apt -y update && apt -y install linux-headers-amd64 zfsutils-linux zfs-dkms zfs-zed
 
 apt-mark hold zfsutils-linux zfs-zed zfs-dkms
-
-dpkg-reconfigure --force zfs-dkms
 
 echo "--- End zfs.sh ---"
